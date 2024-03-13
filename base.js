5b7ec5a13595817dce0921651546f269080fca90
J-IOProgrammer <fg.bonny@outlook.com>
2024-03-13 23:28:57 +0330

    work with events

--- base.js
+++ base.js
@@ -5,3 +5,11 @@ var Person = {
         return this.name + " " + this.family;
     }
 };
+
+function changeText() {
+    document.getElementById('result-tag').innerHTML = 'Text is changed';
+}
+
+function changeInputValue(){
+    document.getElementById('show-text-input-value').innerHTML = Math.random();
+}
