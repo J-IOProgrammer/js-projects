f379ed34713234659b7efc515577473f2b91875a
J-IOProgrammer <fg.bonny@outlook.com>
2024-03-24 17:05:18 +0430

    add base

--- base.js
+++ base.js
@@ -1,15 +1,14 @@
 var Person = {
-    name: "fatemeh",
-    family: "mirfotohi",
-    fullname: function () {
-        return this.name + " " + this.family;
-    }
+  name: "fatemeh",
+  family: "mirfotohi",
+  fullname: function () {
+    return this.name + " " + this.family;
+  },
 };
-
 function changeText() {
-    document.getElementById('result-tag').innerHTML = 'Text is changed';
+  document.getElementById("result-tag").innerHTML = "Text is changed";
 }
 
-function changeInputValue(){
-    document.getElementById('show-text-input-value').innerHTML = Math.random();
+function changeInputValue() {
+  document.getElementById("show-text-input-value").innerHTML = Math.random();
 }
