d84c3a02540f6904c84d77a0b93dcdf555882582
J-IOProgrammer <fg.bonny@outlook.com>
2024-03-25 00:46:58 +0430

    add scripts

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function ToggleMenu() {
+  const toggleMenu = document.querySelector(".menu");
+  // toggle = by every click, this class will add and remove
+  toggleMenu.classList.toggle("active");
+}
