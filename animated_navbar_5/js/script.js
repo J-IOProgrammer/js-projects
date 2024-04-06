8763122b00f76e3d689af4d24838a8bc7cff0b78
J-IOProgrammer <fg.bonny@outlook.com>
2024-04-06 22:44:45 +0430

    add x icon png with style

--- animated_navbar_5/js/script.js
+++ animated_navbar_5/js/script.js
@@ -2,4 +2,5 @@ function toggleMenu() {
     let nav = document.querySelector('#nav');
     let toggle_icon = document.querySelector('#toggle');
     nav.classList.toggle('active');
+    toggle.classList.toggle('active');
 }
