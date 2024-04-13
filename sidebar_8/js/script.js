e7dc8a07005e7ce0ff9e8a29528a3c81acf5cc29
J-IOProgrammer <fg.bonny@outlook.com>
2024-04-13 21:11:26 +0430

    add visibility

--- sidebar_8/js/script.js
+++ sidebar_8/js/script.js
@@ -2,4 +2,5 @@ const btn = document.querySelector('.btn');
 const sidebar = document.querySelector('.sidebar');
 btn.addEventListener('click' , function() {
     sidebar.classList.toggle('active');
+    sidebar.style.visibility = 'visible';
 });
