e3d15d26ee8693a4e5088002deb9e01ecebdbfe6
J-IOProgrammer <fg.bonny@outlook.com>
2024-04-13 21:07:24 +0430

    add animation and script

---
+++ sidebar_8/js/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.btn');
+const sidebar = document.querySelector('.sidebar');
+btn.addEventListener('click' , function() {
+    sidebar.classList.toggle('active');
+});
