d32b0964efe57260cd18beae09976c5baba652c2
J-IOProgrammer <fg.bonny@outlook.com>
2024-04-15 21:25:33 +0430

    add class and style

--- hide_password_11/js/script.js
+++ hide_password_11/js/script.js
@@ -4,7 +4,9 @@ const eye_icon = document.querySelector('.toggle');
 function changeEyeIcon() {
     if(password_input.type === 'password') {
         password_input.setAttribute('type' , 'text');
+        eye_icon.classList.add('hide');
     } else {
         password_input.setAttribute('type' , 'password');
+        eye_icon.classList.remove('hide');
     }
 }
