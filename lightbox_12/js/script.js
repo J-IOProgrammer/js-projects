2c8c923f925513fdc3f763a950c7da4274b0d985
J-IOProgrammer <fg.bonny@outlook.com>
2024-04-16 19:26:03 +0430

    add loop

--- lightbox_12/js/script.js
+++ lightbox_12/js/script.js
@@ -1,4 +1,9 @@
-const imageBox = document.querySelector('.imageBox');
-imageBox.addEventListener('click' , function(){
+const imageBox = document.querySelectorAll('.imageBox');
+
+/* imageBox.addEventListener('click' , function(){
     imageBox.classList.toggle('active');
-});
+}); */
+
+imageBox.forEach(box => box.addEventListener('click' , function(){
+    box.classList.toggle('active');
+}))
