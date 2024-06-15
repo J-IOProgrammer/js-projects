2460b6a4854649c495bc6a23049ef159a2c57ebb
J-IOProgrammer <ermacfe2@gmail.com>
2024-06-15 22:51:45 +0330

    add functions

--- slider_25/js/script.js
+++ slider_25/js/script.js
@@ -1,4 +1,11 @@
-let items = document.querySelector('.slider-item').children;
-let nextSlide = document.querySelector('.right-slide');
-let backSlide = document.querySelector('.left-slide');
+let items = document.querySelector(".slider-item").children;
+let nextSlide = document.querySelector(".right-slide");
+let backSlide = document.querySelector(".left-slide");
 
+nextSlide.onclick = function () {
+  next();
+};
+
+backSlide.onclick = function () {
+  next();
+};
