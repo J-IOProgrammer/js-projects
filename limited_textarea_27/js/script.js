81190f3e7634703d469f525516ac3104387b157f
J-IOProgrammer <ermacfe2@gmail.com>
2024-06-20 21:46:42 +0330

    check 0 char

--- limited_textarea_27/js/script.js
+++ limited_textarea_27/js/script.js
@@ -1,7 +1,10 @@
-const textarea = document.querySelector('#textarea');
-const textarea_text_warning = document.querySelector('#textarea-warning');
+const textarea = document.querySelector("#textarea");
+const textarea_text_warning = document.querySelector("#textarea-warning");
 let MAX_CHARS = 250;
 
-textarea.addEventListener('input', () => {
-    const remaining_word = MAX_CHARS - textarea.value.length;
-})
+textarea.addEventListener("input", () => {
+  const remaining_word = MAX_CHARS - textarea.value.length;
+  if (remaining_word === 0) {
+    textarea_text_warning.textContent = `You are not allowed to write much more! ${remaining_word}`;
+  }
+});
