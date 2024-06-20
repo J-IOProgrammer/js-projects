1f3979e5ed8f9245c53525f0e97b8eee55687483
J-IOProgrammer <ermacfe2@gmail.com>
2024-06-20 22:17:25 +0330

    complete calculating and changing color

--- limited_textarea_27/js/script.js
+++ limited_textarea_27/js/script.js
@@ -1,13 +1,16 @@
 const textarea = document.querySelector("#textarea");
 const textarea_text_warning = document.querySelector("#textarea-warning");
-let remaining_content = 'characters';
 let MAX_CHARS = 250;
 
 textarea.addEventListener("input", () => {
   const remaining_word = MAX_CHARS - textarea.value.length;
+  
+  const remaning_color = remaining_word < 25 ? 'red' : null;
+  console.log(remaning_color);
+  textarea_text_warning.textContent = `${remaining_word} characters`;
+  textarea_text_warning.style.color = remaning_color;
+
   if (remaining_word === 0) {
-    textarea_text_warning.textContent = `You are not allowed to write much more! ${remaining_word}`;
-  } else {
-    textarea_text_warning.textContent = remaining_word + ' ' + remaining_content;
+    textarea_text_warning.textContent = `You are not allowed to write much more!`;
   }
 });
