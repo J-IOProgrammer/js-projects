615cc447c26115ccf39e9aafd7a34b7b47b2256f
J-IOProgrammer <ermacfe2@gmail.com>
2024-06-20 21:55:57 +0330

    change number of char

--- limited_textarea_27/js/script.js
+++ limited_textarea_27/js/script.js
@@ -1,10 +1,13 @@
 const textarea = document.querySelector("#textarea");
 const textarea_text_warning = document.querySelector("#textarea-warning");
+let remaining_content = 'characters';
 let MAX_CHARS = 250;
 
 textarea.addEventListener("input", () => {
   const remaining_word = MAX_CHARS - textarea.value.length;
   if (remaining_word === 0) {
     textarea_text_warning.textContent = `You are not allowed to write much more! ${remaining_word}`;
+  } else {
+    textarea_text_warning.textContent = remaining_word + ' ' + remaining_content;
   }
 });
