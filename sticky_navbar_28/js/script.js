b7015ee24957428d1d32ff0d2f26d23a600e4843
J-IOProgrammer <ermacfe2@gmail.com>
2024-06-29 21:30:30 +0330

    add sticky class

--- sticky_navbar_28/js/script.js
+++ sticky_navbar_28/js/script.js
@@ -1,16 +1,8 @@
-const textarea = document.querySelector("#textarea");
-const textarea_text_warning = document.querySelector("#textarea-warning");
-let MAX_CHARS = 250;
+let header = document.querySelector(".header");
+let sticky = header.offsetTop;
 
-textarea.addEventListener("input", () => {
-  const remaining_word = MAX_CHARS - textarea.value.length;
-  
-  const remaning_color = remaining_word < 25 ? 'red' : null;
-  console.log(remaning_color);
-  textarea_text_warning.textContent = `${remaining_word} characters`;
-  textarea_text_warning.style.color = remaning_color;
-
-  if (remaining_word === 0) {
-    textarea_text_warning.textContent = `You are not allowed to write much more!`;
+window.addEventListener("scroll", () => {
+  if (window.scrollY > sticky) {
+    header.classList.add('sticky');
   }
 });
