de60d2dfd4adac72650d812686d98842bd7aafbb
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-04 21:42:45 +0330

    remove active class

--- customers_comments_slides_29/js/script.js
+++ customers_comments_slides_29/js/script.js
@@ -3,6 +3,9 @@ const indicator = document.querySelector(".slide-indicator").children;
 
 for (i = 0; i < indicator.length; i++) {
   indicator[i].addEventListener("click", function () {
+    for (j = 0; j < indicator.length; j++) {
+      indicator[j].classList.remove("active");
+    }
     this.classList.add("active");
   });
 }
