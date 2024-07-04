423cf39e1230cada18f72920acf629f005915271
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-04 21:54:34 +0330

    complete changing img and comments

--- customers_comments_slides_29/js/script.js
+++ customers_comments_slides_29/js/script.js
@@ -1,11 +1,18 @@
-const sliders = document.querySelector(".slider").children;
+const slider = document.querySelector(".slider").children;
 const indicator = document.querySelector(".slide-indicator").children;
 
 for (i = 0; i < indicator.length; i++) {
+    
   indicator[i].addEventListener("click", function () {
     for (j = 0; j < indicator.length; j++) {
       indicator[j].classList.remove("active");
     }
     this.classList.add("active");
+
+    const id = this.getAttribute('data-id');
+    for (z = 0; z < slider.length; z++) {
+        slider[z].classList.remove("active");
+      }
+    slider[id].classList.add("active");
   });
 }
