07f34b354d31d45b0322086361e362763d84dcfe
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-13 20:04:46 +0330

    using sectionHeight for scroll

--- smooth_scroll_30/js/script.js
+++ smooth_scroll_30/js/script.js
@@ -6,15 +6,15 @@ window.addEventListener("scroll", function () {
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.clientHeight;
-    if (this.scrollY >= sectionTop) {
+    if (this.scrollY >= sectionTop - sectionHeight / 3) {
       currentId = section.getAttribute("id");
     }
   });
 
   li_tags.forEach((li) => {
-    li.classList.remove('active');
-    if(li.classList.contains(currentId)){
-        li.classList.add('active');
+    li.classList.remove("active");
+    if (li.classList.contains(currentId)) {
+      li.classList.add("active");
     }
   });
 });
