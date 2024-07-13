e77a6e8d4d98c1def64a676d1d9e6db61dfffa08
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-13 19:53:43 +0330

    add active class to active li

--- smooth_scroll_30/js/script.js
+++ smooth_scroll_30/js/script.js
@@ -1,13 +1,20 @@
-const sections = document.querySelectorAll('section');
-const li_tags = document.querySelectorAll('nav .container ul li');
+const sections = document.querySelectorAll("section");
+const li_tags = document.querySelectorAll("nav .container ul li");
 let currentId;
 
-window.addEventListener('scroll', function(){
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if(this.scrollY >= sectionTop) {
-            currentId = section.getAttribute('id');
-        }
-    })
-})
+window.addEventListener("scroll", function () {
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (this.scrollY >= sectionTop) {
+      currentId = section.getAttribute("id");
+    }
+  });
+
+  li_tags.forEach((li) => {
+    li.classList.remove('active');
+    if(li.classList.contains(currentId)){
+        li.classList.add('active');
+    }
+  });
+});
