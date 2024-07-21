1da6b4de8aa7b3277518380d05e83a69e2f21513
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-21 22:03:52 +0330

    move box

--- move_hover_32/js/script.js
+++ move_hover_32/js/script.js
@@ -1,13 +1,22 @@
-const boxes = document.querySelector('.content').children;
-const box_width = boxes[0].querySelector('.inner').offsetWidth;
-const box_height = boxes[0].querySelector('.inner').offsetHeight;
+const boxes = document.querySelector(".content").children;
+const box_width = boxes[0].querySelector(".inner").offsetWidth;
+const box_height = boxes[0].querySelector(".inner").offsetHeight;
 
-const div_element = document.createElement('div');
+const div_element = document.createElement("div");
 div_element.style.position = "absolute";
 div_element.style.width = box_width + "px";
 div_element.style.height = box_height + "px";
 div_element.style.backgroundColor = "yellow";
-div_element.style.left = boxes[0].querySelector('.inner').offsetLeft + "px";
-div_element.style.top = boxes[0].querySelector('.inner').offsetTop + "px";
+div_element.style.left = boxes[0].querySelector(".inner").offsetLeft + "px";
+div_element.style.top = boxes[0].querySelector(".inner").offsetTop + "px";
 
-document.querySelector('.grid-box').appendChild(div_element);
+document.querySelector(".grid-box").appendChild(div_element);
+
+for (let i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener("click", function () {
+    const x = this.offsetLeft;
+    const y = this.offsetTop;
+    div_element.style.left = x + "px";
+    div_element.style.top = y + "px";
+  });
+}
