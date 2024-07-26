487e6b22a7c32b541f9894bb8ba7f99c3f1bda8b
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-26 18:56:19 +0330

    add click event for close btn

--- custom_alarm_33/js/script.js
+++ custom_alarm_33/js/script.js
@@ -3,22 +3,26 @@ const close_btn = document.querySelector(".close-btn");
 const alert_box = document.querySelector(".alert-box");
 let timer;
 
+close_btn.addEventListener("click", function () {
+    hideAlertBox();
+});
+
 click_btn.addEventListener("click", function () {
   showAlertBox();
 });
 
 function showAlertBox() {
-    alert_box.classList.remove('hide');
-    alert_box.classList.add('show');
-    if(alert_box.classList.contains('hidden')) {
-        alert_box.classList.remove('hidden');
-    }
-    timer = setTimeout(() => {
-        hideAlertBox();
-    }, 5000);
+  alert_box.classList.remove("hide");
+  alert_box.classList.add("show");
+  if (alert_box.classList.contains("hidden")) {
+    alert_box.classList.remove("hidden");
+  }
+  timer = setTimeout(() => {
+    hideAlertBox();
+  }, 5000);
 }
 
 function hideAlertBox() {
-    alert_box.classList.remove('show');
-    alert_box.classList.add('hide');
+  alert_box.classList.remove("show");
+  alert_box.classList.add("hide");
 }
