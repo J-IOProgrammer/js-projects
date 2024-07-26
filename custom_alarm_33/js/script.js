178220511afefc1c157990e2d331758a8cc53874
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-26 18:29:52 +0330

    add hideAlertBox func

--- custom_alarm_33/js/script.js
+++ custom_alarm_33/js/script.js
@@ -1,6 +1,7 @@
 const click_btn = document.querySelector(".click-btn");
 const close_btn = document.querySelector(".close-btn");
 const alert_box = document.querySelector(".alert-box");
+let timer;
 
 click_btn.addEventListener("click", function () {
   showAlertBox();
@@ -10,4 +11,11 @@ function showAlertBox() {
     if(alert_box.classList.contains('hidden')) {
         alert_box.classList.remove('hidden');
     }
+    timer = setTimeout(() => {
+        hideAlertBox();
+    }, 5000);
+}
+
+function hideAlertBox() {
+    alert_box.classList.add('hide');
 }
