0d705389cd73a5908e4225539a766ffb43d4d505
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-30 22:40:48 +0330

    get uploaded file

--- preview_image_35/js/script.js
+++ preview_image_35/js/script.js
@@ -1,4 +1,8 @@
-const input_btn = document.querySelector('#input-file');
-const image_preview_div = document.querySelector('#image-preview');
-const uploaded_image = image_preview_div.querySelector('.uploaded-image');
-const text_prev = image_preview_div.querySelector('.text-prev');
+const input_btn = document.querySelector("#input-file");
+const image_preview_div = document.querySelector("#image-preview");
+const uploaded_image = image_preview_div.querySelector(".uploaded-image");
+const text_prev = image_preview_div.querySelector(".text-prev");
+
+input_btn.addEventListener("change", function () {
+    const file = this.files[0];
+});
