e0adfa3225264947ec61dd2d9b093b9c4e9e9434
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-30 22:36:27 +0330

    get elements

---
+++ preview_image_35/js/script.js
@@ -0,0 +1,4 @@
+const input_btn = document.querySelector('#input-file');
+const image_preview_div = document.querySelector('#image-preview');
+const uploaded_image = image_preview_div.querySelector('.uploaded-image');
+const text_prev = image_preview_div.querySelector('.text-prev');
