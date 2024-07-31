9913e9de3c1321e33f838191247122f10c3e234a
J-IOProgrammer <ermacfe2@gmail.com>
2024-07-31 23:09:47 +0330

    show image

--- preview_image_35/js/script.js
+++ preview_image_35/js/script.js
@@ -1,15 +1,22 @@
 const input_btn = document.querySelector("#input-file");
-const image_preview_div = document.querySelector("#image-preview");
+const image_preview_div = document.querySelector("#image-preview-div");
 const uploaded_image = image_preview_div.querySelector(".uploaded-image");
 const text_prev = image_preview_div.querySelector(".text-prev");
 
 input_btn.addEventListener("change", function () {
-    const file = this.files[0];
+  const file = this.files[0];
 
-    if(file) {
-        const reader = new FileReader();
-        text_prev.computedStyleMap.display = 'none';
-        uploaded_image.style.display = "block";
-        
-    }
+  if (file) {
+    const reader = new FileReader();
+
+    text_prev.computedStyleMap.display = "none";
+    uploaded_image.style.display = "block";
+
+    reader.addEventListener("load", function () {
+      uploaded_image.setAttribute("src", this.result);
+    });
+
+    reader.readAsDataURL(file);
+
+  }
 });
