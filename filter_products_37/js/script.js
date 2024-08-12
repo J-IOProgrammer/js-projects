944d33f5e6d188cc9a238d767d93dc4d5da59a81
J-IOProgrammer <ermacfe2@gmail.com>
2024-08-12 22:37:07 +0330

    change active class between tabs

--- filter_products_37/js/script.js
+++ filter_products_37/js/script.js
@@ -1,2 +1,11 @@
-const indicator = document.querySelector('.indicator').children;
-const main = document.querySelector('.items').children;
+const indicator = document.querySelector(".indicator").children;
+const main = document.querySelector(".items").children;
+
+for (let i = 0; i < indicator.length; i++) {
+  indicator[i].onclick = function () {
+    for (let j = 0; j < indicator.length; j++) {
+      indicator[j].classList.remove("active");
+    }
+    this.classList.add("active");
+  };
+}
