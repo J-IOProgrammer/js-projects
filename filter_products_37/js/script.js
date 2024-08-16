a139a1536c8f909aa7f234ac19bf50e136978d5b
J-IOProgrammer <ermacfe2@gmail.com>
2024-08-16 19:30:21 +0330

    complete filterring items

--- filter_products_37/js/script.js
+++ filter_products_37/js/script.js
@@ -7,5 +7,12 @@ for (let i = 0; i < indicator.length; i++) {
       indicator[j].classList.remove("active");
     }
     this.classList.add("active");
+    const displayItems = this.getAttribute("data-filter");
+    for (let k = 0; k < main.length; k++) {
+      main[k].style.display = "none";
+      if(main[k].getAttribute('data-category') == displayItems){
+        main[k].style.display = "block";
+      }
+    }
   };
 }
