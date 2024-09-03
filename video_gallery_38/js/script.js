eb8fa0a91d6adde3be5e1d505f50ccbc6e1e707d
J-IOProgrammer <ermacfe2@gmail.com>
2024-09-03 22:33:38 +0330

    add closeVideo function

--- video_gallery_38/js/script.js
+++ video_gallery_38/js/script.js
@@ -1,8 +1,11 @@
 const video_player = document.querySelector("#videoPlayer");
 const video_box = document.querySelector("#video-box");
 
-function changeVideoSource(video_path) {
-    video_box.src = video_path;
-  video_player.style.display = "block";
+function closeVideo() {
+    video_player.style = 'none';
+}
 
+function changeVideoSource(video_path) {
+  video_box.src = video_path;
+  video_player.style.display = "block";
 }
