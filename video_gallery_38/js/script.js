145b6b436d2a455a26c1b266c01439944a69d9c2
J-IOProgrammer <ermacfe2@gmail.com>
2024-09-03 22:34:45 +0330

    add display

--- video_gallery_38/js/script.js
+++ video_gallery_38/js/script.js
@@ -2,7 +2,7 @@ const video_player = document.querySelector("#videoPlayer");
 const video_box = document.querySelector("#video-box");
 
 function closeVideo() {
-    video_player.style = 'none';
+    video_player.style.display = 'none';
 }
 
 function changeVideoSource(video_path) {
