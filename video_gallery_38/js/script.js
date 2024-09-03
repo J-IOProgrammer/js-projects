f84c337c70162a78dc4eaf2d3f09124738ae0636
J-IOProgrammer <ermacfe2@gmail.com>
2024-09-03 22:02:55 +0330

    add changeVideoSource function

--- video_gallery_38/js/script.js
+++ video_gallery_38/js/script.js
@@ -1,1 +1,6 @@
-const video_player = document.querySelector('#videoPlayer');
+const video_player = document.querySelector("#videoPlayer");
+const video_box = document.querySelector("#video-box");
+
+function changeVideoSource() {
+  video_player.style.display = "block";
+}
