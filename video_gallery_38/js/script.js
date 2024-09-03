ae79e762616f4ed95333b20104dd035547217dde
J-IOProgrammer <ermacfe2@gmail.com>
2024-09-03 22:26:11 +0330

    change source

--- video_gallery_38/js/script.js
+++ video_gallery_38/js/script.js
@@ -1,6 +1,8 @@
 const video_player = document.querySelector("#videoPlayer");
 const video_box = document.querySelector("#video-box");
 
-function changeVideoSource() {
+function changeVideoSource(video_path) {
+    video_box.src = video_path;
   video_player.style.display = "block";
+
 }
