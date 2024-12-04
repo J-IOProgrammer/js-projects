3cf58cb5e4944bee789d67804d2dc563010460f4
J-IOProgrammer <fatemehmirfotohi125@gmail.com>
2024-12-04 22:03:56 +0330

    get elements

---
+++ popup_searchbox_42/js/script.js
@@ -0,0 +1,4 @@
+const search_btn = document.querySelector('.search-icon');
+const overlay = document.querySelector('.search-overlay');
+const search_poup = document.querySelector('.search-popup');
+
