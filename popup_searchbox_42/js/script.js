9d6f16ef6247956e9f0ad0920094bdcbb6520066
J-IOProgrammer <fatemehmirfotohi125@gmail.com>
2024-12-04 22:07:56 +0330

    add active class by click

--- popup_searchbox_42/js/script.js
+++ popup_searchbox_42/js/script.js
@@ -1,4 +1,8 @@
-const search_btn = document.querySelector('.search-icon');
-const overlay = document.querySelector('.search-overlay');
-const search_poup = document.querySelector('.search-popup');
+const search_btn = document.querySelector(".search-icon");
+const overlay = document.querySelector(".search-overlay");
+const search_poup = document.querySelector(".search-popup");
 
+search_btn.addEventListener("click", function () {
+    overlay.classList.add('active');
+    search_poup.classList.add('active');
+});
