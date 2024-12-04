7d9b61421a8ba5d25b192c7295f335acb0618fa4
J-IOProgrammer <fatemehmirfotohi125@gmail.com>
2024-12-04 22:13:59 +0330

    remove actvie class

--- popup_searchbox_42/js/script.js
+++ popup_searchbox_42/js/script.js
@@ -3,6 +3,11 @@ const overlay = document.querySelector(".search-overlay");
 const search_poup = document.querySelector(".search-popup");
 
 search_btn.addEventListener("click", function () {
-    overlay.classList.add('active');
-    search_poup.classList.add('active');
+  overlay.classList.add("active");
+  search_poup.classList.add("active");
+});
+
+overlay.addEventListener("click", function () {
+    overlay.classList.remove("active");
+  search_poup.classList.remove("active");
 });
