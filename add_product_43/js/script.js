f4649c9aeeeccbf7966d3cb79facb0a097f93181
J-IOProgrammer <fatemehmirfotohi125@gmail.com>
2025-02-02 22:25:36 +0330

    clonning items and add to select

--- add_product_43/js/script.js
+++ add_product_43/js/script.js
@@ -3,9 +3,18 @@ const select = document.querySelector(".select");
 const buttons = document.getElementsByTagName("button");
 
 for (button of buttons) {
-  button.addEventListener("click", () => {
+  button.addEventListener("click", (event) => {
     const add = Number(notification.getAttribute("data-count") || 0);
     notification.setAttribute("data-count", add + 1);
-    notification.classList.add('zero');
+    notification.classList.add("zero");
+
+    let parent = event.target.parentNode;
+    let clone = parent.cloneNode(true);
+    select.appendChild(clone);
+    if (clone) {
+      notification.onclick = () => {
+        select.classList.toggle('display');
+      };
+    }
   });
 }
