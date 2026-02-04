600decc9a60181e67dedb90b116712fa61b6cd7c
J-IOProgrammer <fatemehmirfotohi125@gmail.com>
2026-02-05 00:07:05 +0330

    add active class to logos

--- welcome_page_46/js/script.js
+++ welcome_page_46/js/script.js
@@ -1,6 +1,16 @@
 const intro = document.querySelector('.intro')
+const logos = document.querySelectorAll('.logo')
 
 window.addEventListener('load', function() {
+
+    this.setTimeout(()=> {
+        logos.forEach(logo => {
+            this.setTimeout(()=> {
+                logo.classList.add('active')
+            })
+        })
+    })
+
     this.setTimeout(()=> {
         intro.style.top = '-100vh'
     }, 2000)
