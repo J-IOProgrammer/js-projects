b1936faf88e76ab158a54a225c3b800ef729433a
J-IOProgrammer <fatemehmirfotohi125@gmail.com>
2026-03-14 00:06:34 +0330

    toggle hide class for social icons

--- animated_card_47/js/script.js
+++ animated_card_47/js/script.js
@@ -1,5 +1,7 @@
 const profileImg = document.querySelector('img')
+const socialIcons = document.querySelector('.icon')
 
 profileImg.onclick = () => {
     profileImg.classList.toggle('active')
+    socialIcons.classList.toggle('hide')
 }
